6589198f6106d2a1d8ae86c9bf09df1a20e90e5a
ArmanKamal <armankamal2421@gmail.com>
2021-04-28 18:22:58 -0500

    Template & Auth app added

---
+++ authentication/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import SignUpView
+
+urlpatterns = [
+    path('register', SignUpView.as_view(),name="register"),
+    
+]
--- expensetracker/urls.py
+++ expensetracker/urls.py
@@ -4,5 +4,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('expenses.urls'))
+    path('',include('expenses.urls')),
+    path('auth/',include('authentication.urls'))
 ]
