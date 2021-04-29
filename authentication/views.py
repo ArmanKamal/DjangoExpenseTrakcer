559a99c4c3721d4493f68f54c4407bdfce3883ad
ArmanKamal <armankamal2421@gmail.com>
2021-04-29 17:55:54 -0500

    Email Validation Added

--- authentication/views.py
+++ authentication/views.py
@@ -3,7 +3,18 @@ from django.views import View
 import json
 from django.http import JsonResponse
 from django.contrib.auth.models import User
+from validate_email import validate_email
 # Create your views here.
+
+class EmailValidation(View):
+    def post(self,request):
+        data = json.loads(request.body)
+        email = data['email']
+        if not validate_email(email):
+            return JsonResponse({'email_error': 'Invalid Email'},status=400)
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({'email_error': 'Email already exists, choose another one'},status=409)
+        return JsonResponse({'email_valid':True},status=200)
 
 class UsernameValidationView(View):
     def post(self,request):
--- expensetracker/static/js/register.js
+++ expensetracker/static/js/register.js
@@ -1,8 +1,7 @@
 let usernameField = document.getElementById('username')
 let invalidUserNameField = document.querySelector('.invalid-username')
-
-
-
+let emailField = document.getElementById('email')
+let invalidEmail = document.querySelector('.invalid-email')
 
 usernameField.addEventListener('keyup', function(e){
     usernameVal = e.target.value
@@ -29,4 +28,32 @@ usernameField.addEventListener('keyup', function(e){
         })
     }
     
+})
+
+
+emailField.addEventListener('keyup',function(e){
+    emailVal = e.target.value
+    if(emailVal.length>0){
+        fetch('/auth/validate-email',{
+            headers : { 
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({email: emailVal}),
+            method: "POST",
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            if(data.email_error){
+                emailField.classList.add("is-invalid");
+                invalidEmail.style.display = "block" ;
+                invalidEmail.innerHTML=`<p class="text-danger">${data.email_error}</p>`
+           
+            }
+            else{
+                emailField.classList.remove("is-invalid");
+                invalidEmail.style.display = "none" ;
+            }
+        })
+    }
+
 })
