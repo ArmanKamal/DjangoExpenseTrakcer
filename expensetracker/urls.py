367bad78cad0f0f814b20055f8bd43f2b053c524
ArmanKamal <armankamal2421@gmail.com>
2021-05-17 20:22:01 -0500

    Added Currency json

--- expensetracker/urls.py
+++ expensetracker/urls.py
@@ -5,5 +5,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('expenses.urls')),
+    path('users/',include('usersettings.urls')),
     path('auth/',include('authentication.urls'))
 ]
--- usersettings/views.py
+++ usersettings/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+import json
+import os
+from django.conf import settings
 # Create your views here.
+def index(request):
+    currency_data = []
+    path = os.path.join(settings.BASE_DIR, 'currencies.json')
+    with open(path) as json_file:
+        data = json.load(json_file)
+
+        for key,item in data.items():
+            currency_data.append({'name':key, 'value':item})
+    context = {
+        'currency_data': currency_data
+    }
+
+    return render(request, 'settings/index.html')
