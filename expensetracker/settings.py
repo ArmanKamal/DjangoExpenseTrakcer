cdd9ea15a9dc5ec5e01a6d4843bdf0fea0b780c7
ArmanKamal <armankamal2421@gmail.com>
2021-07-09 19:40:34 -0500

    email settings changed

--- expensetracker/settings.py
+++ expensetracker/settings.py
@@ -144,6 +144,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
 
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
